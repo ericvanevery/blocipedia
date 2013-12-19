d092a5e2f38d25921a8c31c34755d7a4ab54443c
skillbilly <vanevery.eric@gmail.com>
2013-12-19 16:01:52 -0500

    fc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = '2cb6d59eb1f0ac7acf0b7f42585284c66029e872957ac689e3b255d13b857160ed0bc76c057043f1216a321e23f893913dd4cc20ab697e3032e90ad472dbc8e9'
